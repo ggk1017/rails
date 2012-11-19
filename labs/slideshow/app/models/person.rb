abb4789a7a258c132237ae332a2d74546496ddd6
Gaurav Karnani <gaurav.karnani@gmail.com>
2012-11-19 13:33:48 -0500

    update

---
+++ labs/slideshow/app/models/person.rb
@@ -0,0 +1,5 @@
+class Person < ActiveRecord::Base
+	validates :name, :presence => true
+  mount_uploader :photo, ProfilePicUploader
+end
+
---
+++ labs/slideshow/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Slideshow::Application.config.secret_token = '87de3f648ff827a879a25bb62dbc5709fbde38522e379fe6fa6566a3ef44178392c796673c63e345d721907d5f5d01573544586ae328a0a21dc9b40c723ccf78'
