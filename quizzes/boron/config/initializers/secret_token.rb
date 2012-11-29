a1800493c9e1acd7e737c6ff5d9586fe17146242
Gaurav Karnani <gaurav.karnani@gmail.com>
2012-11-29 12:28:51 -0500

    quizzes

---
+++ quizzes/boron/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Boron::Application.config.secret_token = '39e7b7e4452ad614e2b5c684cedc2c988b68bdc20c818abb081251aee99e48bfcebeebd86b288132163924c587ee5fe7ebc5662dcafccc780f502bed35b63091'
